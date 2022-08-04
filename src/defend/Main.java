8e6d8946619d3a90f9eef5b46d6af655027a8493
Tien Dao <tienminh1351@gmail.com>
2022-08-04 12:17:00 -0700

    Upload get int value and file name from user

--- src/defend/Main.java
+++ src/defend/Main.java
@@ -12,48 +12,67 @@ public class Main {
   private static Scanner IN = new Scanner(System.in);
   private static Pattern PATTERN; 
   private static Matcher MATCHER;
-  private static final String REGEXNAME = "[ \\s\\W\\d]";
   
   /**
-   * Method get user last name, also check first name legitimate.
+   * Method get user name, also check name legitimate
+   * Not allow non alphabet word character, whitespace and empty space.
    */
-  public static String promtFirstName() {
-    System.out.print("Enter Your First Name:");
-    String firstName = IN.nextLine();
-    PATTERN = Pattern.compile(REGEXNAME);
-    MATCHER = PATTERN.matcher(firstName);
+  public static String promtName(final String firstorlast) {
+    final String regex = "[\\s\\W\\d]";
+    System.out.print("Enter Your " + firstorlast + " Name:");
+    final String name = IN.nextLine();
+    PATTERN = Pattern.compile(regex);
+    MATCHER = PATTERN.matcher(name);
     
-    if (MATCHER.find() || firstName.length() > 50 || firstName.isEmpty()) {
+    if (MATCHER.find() || name.length() > 50 || name.isEmpty()) {
       System.out.println("invalid name please re-enter");
-      promtFirstName();
+      promtName(firstorlast);
     }
-    return firstName;
-  }
-  
-  /**
-   * Method get user last name, also check last name legitimate.
-   */
-  public static String promtLastName() {
-    System.out.print("Enter Your Last Name:");
-    String lastName = IN.nextLine();
-    MATCHER = PATTERN.matcher(lastName);
-    if (MATCHER.find() || lastName.length() > 50 || lastName.isEmpty()) {
-      System.out.println("invalid name please re-enter");
-      promtLastName();
-    } 
-    return lastName;
+    return name;
   }
 
   //prompts for and reads in two int values from the user (range of values are those of a 4 byte int)
   public static int promtInt() {
-    return 0;
+    System.out.print("Enter your integer: ");
+    try {
+        final int intInput = Integer.parseInt(IN.nextLine());
+        return intInput;
+    } catch (Exception e) {
+        System.out.println("Invalid input. Please try again.");
+        return promtInt();
+    }
   }
+  
+  //prompts for reads the name of an input file from the user
+  //prompts for reads the name of an output file from the user
+  public static void promtFileName() {
+    System.out.println("Enter Name For File:");
+    final String regex = "[a-z[\\d]]";
+    final String name = IN.nextLine();
+    PATTERN = Pattern.compile(regex);
+    MATCHER = PATTERN.matcher(name);
+    if (!MATCHER.find() || name.length() > 50 || name.isEmpty()) {
+      System.out.println("invalid name please re-enter");
+      promtFileName();
+    }
+  }
+  
   /**
    * Main method.
    */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    promtFirstName();
-    promtLastName();
+//    promtName("First");
+//    
+//    promtName("Last");
+//    
+//    promtInt();
+//    
+//    promtInt();
+    
+    promtFileName();
+    
+//    promtFileName("Output");
+    
   }
 }
